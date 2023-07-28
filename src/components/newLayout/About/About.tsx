e3db991f4521f1e36b664693bbf75b4028f48973
Artur Watanabe <arturwatanabe@gmail.com>
2023-07-28 04:42:50 -0300

    add: linux

--- src/components/newLayout/About/About.tsx
+++ src/components/newLayout/About/About.tsx
@@ -146,7 +146,7 @@ export default function About() {
           </h2>
           <h3>
             Atualmente, atuo com React.js, Node.js, TypeScript, Docker,
-            PostgreSQL, MongoDB e estudando React Native e UI/UX.
+            PostgreSQL, MongoDB e estudando React Native, UI/UX e Linux.
           </h3>
         </div>
         <div className="buttons">
